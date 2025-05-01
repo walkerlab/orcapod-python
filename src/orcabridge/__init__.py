a8cf00a91c37b8b29a02a6db59ea31b45723f681
Edgar Y. Walker <eywalker@uw.edu>
2025-05-01 06:54:44 +0000

    feat: add default tracker

---
+++ src/orcabridge/__init__.py
@@ -0,0 +1,4 @@
+from .operation import Tracker
+
+DEFAULT_TRACKER = Tracker()
+DEFAULT_TRACKER.activate()
--- src/orcabridge/operation.py
+++ src/orcabridge/operation.py
@@ -74,6 +74,15 @@ class Tracker():
     def record(self, invocation: Invocation) -> None:
         self.invocation_lut.setdefault(invocation.operation, set()).add(invocation)
 
+    def reset(self) -> Dict[Operation, Collection[Invocation]]:
+        """
+        Reset the tracker and return the recorded invocations.
+        """
+        recorded_invocations = self.invocation_lut
+        self.invocation_lut = {}
+        return recorded_invocations
+        
+
     def generate_namemap(self) -> Dict[Invocation, str]:
         namemap = {}
         for operation, invocations in self.invocation_lut.items():
@@ -86,6 +95,23 @@ class Tracker():
             for idx, invocation in enumerate(invocations):
                 namemap[invocation] = f"{operation}_{idx}"
         return namemap
+    
+    def activate(self) -> None:
+        """
+        Activate the tracker. This is a no-op if the tracker is already active.
+        """
+        if not self.active:
+            if not hasattr(self._local, 'active_trackers'):
+                self._local.active_trackers = []
+            self._local.active_trackers.append(self)
+            self.active = True
+
+    def deactivate(self) -> None:
+        # Remove this tracker from active trackers
+        if hasattr(self._local, 'active_trackers') and self.active:
+            self._local.active_trackers.remove(self)
+            self.active = False
+        
 
     def generate_graph(self):
         G = nx.DiGraph()
@@ -102,20 +128,12 @@ class Tracker():
         return G
 
 
-
     def __enter__(self):
-        if not hasattr(self._local, 'active_trackers'):
-            self._local.active_trackers = []
-
-        self._local.active_trackers.append(self)
-        self.active = True
+        self.activate()
         return self
 
     def __exit__(self, exc_type, exc_val, ext_tb):
-        # Remove this tracker from active trackers
-        if hasattr(self._local, 'active_trackers'):
-            self._local.active_trackers.remove(self)
-        self.active = False
+        self.deactivate()
         
 
     @classmethod
