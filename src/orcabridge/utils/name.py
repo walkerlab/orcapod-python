67e78c74e17e8db4f82cb2c776fc8728daa24038
Edgar Y. Walker <edgar.walker@gmail.com>
2025-05-06 08:25:50 +0000

    fix: patch failure modes of name conversion

--- src/orcabridge/utils/name.py
+++ src/orcabridge/utils/name.py
@@ -14,7 +14,8 @@ def pascal_to_snake(name: str) -> str:
     # if already in snake_case, return as is
     # TODO: replace this crude check with a more robust one
     if "_" in name:
-        return name
+        # put everything into lowercase and return
+        return name.lower()
     # Replace uppercase letters with underscore followed by lowercase letter
     s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
     return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
@@ -24,7 +25,8 @@ def snake_to_pascal(name: str) -> str:
     # Convert snake_case to PascalCase
     # if already in PascalCase, return as is
     if "_" not in name:
-        return name
+        # capitalize the name and return
+        return name.capitalize()
     # Split the string by underscores and capitalize each component
     components = name.split("_")
     return "".join(x.title() for x in components)
