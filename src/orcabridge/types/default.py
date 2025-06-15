c1a614aff5f5877252c3cc72b52179e86382b85f
Edgar Y. Walker <eywalker@uw.edu>
2025-06-15 04:40:41 +0000

    feat: add working implementation of packet converter to arrow

--- src/orcabridge/types/default.py
+++ src/orcabridge/types/default.py
@@ -13,11 +13,6 @@ default_registry = TypeRegistry()
 # Register with semantic names - registry extracts supported types automatically
 default_registry.register("path", PathHandler())
 default_registry.register("uuid", UUIDHandler())
-default_registry.register("int", SimpleMappingHandler(int, pa.int64()))
-default_registry.register("float", SimpleMappingHandler(float, pa.float64()))
-default_registry.register("bool", SimpleMappingHandler(bool, pa.bool_()))
-default_registry.register("str", SimpleMappingHandler(str, pa.string()))
-default_registry.register("bytes", SimpleMappingHandler(bytes, pa.binary()))
 default_registry.register(
     "datetime", DateTimeHandler()
 )  # Registers for datetime, date, time
