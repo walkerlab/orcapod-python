c9a1d3e501bfe0aac020d1994ed31ea4a602e54a
Edgar Y. Walker <eywalker@uw.edu>
2025-06-15 04:39:58 +0000

    refactor: place utils and new data types

--- src/orcabridge/store/types.py
+++ src/orcabridge/store/types.py
@@ -1,6 +1,7 @@
 from typing import Protocol, runtime_checkable
 
 from orcabridge.types import Packet
+import pyarrow as pa
 
 
 @runtime_checkable
@@ -18,6 +19,27 @@ class DataStore(Protocol):
         packet: Packet,
         output_packet: Packet,
     ) -> Packet: ...
+
+    def retrieve_memoized(
+        self, function_name: str, function_hash: str, packet: Packet
+    ) -> Packet | None: ...
+
+
+@runtime_checkable
+class ArrowBasedDataStore(Protocol):
+    """
+    Protocol for data stores that can memoize and retrieve packets.
+    This is used to define the interface for data stores like DirDataStore.
+    """
+
+    def __init__(self, *args, **kwargs) -> None: ...
+    def memoize(
+        self,
+        function_name: str,
+        function_hash: str,
+        packet: pa.Table,
+        output_packet: pa.Table,
+    ) -> pa.Table: ...
 
     def retrieve_memoized(
         self, function_name: str, function_hash: str, packet: Packet
