b04fb61692d376044c8ec6887e90413f79ce7c64
Edgar Y. Walker <eywalker@uw.edu>
2025-07-10 23:49:06 +0000

    refactor: temporarily stop top level import while refactoring

--- src/orcapod/__init__.py
+++ src/orcapod/__init__.py
@@ -1,36 +1,36 @@
-from .core import operators, sources, streams
-from .core.streams import SyncStreamFromLists, SyncStreamFromGenerator
-from . import hashing, stores
-from .core.operators import Join, MapPackets, MapTags, packet, tag
-from .core.pod import FunctionPod, function_pod
-from .core.sources import GlobSource
-from .stores import DirDataStore, SafeDirDataStore
-from .core.tracker import GraphTracker
-from .pipeline import Pipeline
+# from .core import operators, sources, streams
+# from .core.streams import SyncStreamFromLists, SyncStreamFromGenerator
+# from . import hashing, stores
+# from .core.operators import Join, MapPackets, MapTags, packet, tag
+# from .core.pod import FunctionPod, function_pod
+# from .core.sources import GlobSource
+# from .stores import DirDataStore, SafeDirDataStore
+# from .core.tracker import GraphTracker
+# from .pipeline import Pipeline
 
-DEFAULT_TRACKER = GraphTracker()
-DEFAULT_TRACKER.activate()
+# DEFAULT_TRACKER = GraphTracker()
+# DEFAULT_TRACKER.activate()
 
 
-__all__ = [
-    "hashing",
-    "stores",
-    "pod",
-    "operators",
-    "streams",
-    "sources",
-    "MapTags",
-    "MapPackets",
-    "Join",
-    "tag",
-    "packet",
-    "FunctionPod",
-    "function_pod",
-    "GlobSource",
-    "DirDataStore",
-    "SafeDirDataStore",
-    "DEFAULT_TRACKER",
-    "SyncStreamFromLists",
-    "SyncStreamFromGenerator",
-    "Pipeline",
-]
+# __all__ = [
+#     "hashing",
+#     "stores",
+#     "pod",
+#     "operators",
+#     "streams",
+#     "sources",
+#     "MapTags",
+#     "MapPackets",
+#     "Join",
+#     "tag",
+#     "packet",
+#     "FunctionPod",
+#     "function_pod",
+#     "GlobSource",
+#     "DirDataStore",
+#     "SafeDirDataStore",
+#     "DEFAULT_TRACKER",
+#     "SyncStreamFromLists",
+#     "SyncStreamFromGenerator",
+#     "Pipeline",
+# ]
